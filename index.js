8c6a16cdc60ad638fb4197a64aae609c21b6d5f3
Delfrinando <106639884+del-skillsunion@users.noreply.github.com>
2023-05-27 12:32:35 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,38 @@
 module.exports.handler = async (event) => {
+  const respose = {
+    "timestamp": Date.now().toString(),
+    "status_code": 200,
+    "body": "body"
+  }
+  console.log(respose)
   return {
     statusCode: 200,
     body: JSON.stringify(
       {
         message: "Go Serverless v3.0! Your function executed successfully!",
-        input: event,
+        input: respose,
+      },
+      null,
+      2
+    ),
+  };
+};
+
+module.exports.error = async (event) => {
+  const respose = {
+    "timestamp": Date.now().toString(),
+    "status_code": 400,
+    "body": "body error"
+  }
+
+  const err = new Error(respose)
+  
+  return {
+    statusCode: 400,
+    body: JSON.stringify(
+      {
+        message: "error",
+        input: err.message.body,
       },
       null,
       2
